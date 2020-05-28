2f666ad4ffa02e529318dc1eab65fa2446f05bb8
Denis Sz <62932767+VokuN77@users.noreply.github.com>
2020-05-28 03:22:47 +0200

    Add files via upload

--- lib.sql
+++ lib.sql
@@ -1,1 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 28 Maj 2020, 03:03
+-- Wersja serwera: 10.4.11-MariaDB
+-- Wersja PHP: 7.4.5
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `lib`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `autorzy`
+--
+
+CREATE TABLE `autorzy` (
+  `id_autor` int(10) UNSIGNED NOT NULL,
+  `Imię` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Nazwisko` varchar(50) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `autorzy`
+--
+
+INSERT INTO `autorzy` (`id_autor`, `Imię`, `Nazwisko`) VALUES
+(2, 'Dmitrij', 'Głuchowski'),
+(38, 'John R.R.', 'Tolkien'),
+(50, 'Denis', 'Szendzielorz');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ksiazka`
+--
+
+CREATE TABLE `ksiazka` (
+  `id_ksiazki` int(50) UNSIGNED NOT NULL,
+  `id_autor` int(50) UNSIGNED NOT NULL,
+  `id_tytul` int(50) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `ksiazka`
+--
+
+INSERT INTO `ksiazka` (`id_ksiazki`, `id_autor`, `id_tytul`) VALUES
+(9, 2, 7),
+(20, 38, 5),
+(24, 50, 23);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tytuly`
+--
+
+CREATE TABLE `tytuly` (
+  `id_tytul` int(10) UNSIGNED NOT NULL,
+  `tytul` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `ISBN` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `tytuly`
+--
+
+INSERT INTO `tytuly` (`id_tytul`, `tytul`, `ISBN`) VALUES
+(5, 'Hobbit, czyli tam i z powrotem', 23457),
+(7, 'Metro 2033', 43656),
+(23, 'Php', 32456);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `autorzy`
+--
+ALTER TABLE `autorzy`
+  ADD PRIMARY KEY (`id_autor`),
+  ADD UNIQUE KEY `id_autor_2` (`id_autor`);
+
+--
+-- Indeksy dla tabeli `ksiazka`
+--
+ALTER TABLE `ksiazka`
+  ADD PRIMARY KEY (`id_ksiazki`),
+  ADD KEY `aut` (`id_autor`),
+  ADD KEY `tyt` (`id_tytul`);
+
+--
+-- Indeksy dla tabeli `tytuly`
+--
+ALTER TABLE `tytuly`
+  ADD PRIMARY KEY (`id_tytul`),
+  ADD KEY `id_tytul` (`id_tytul`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `autorzy`
+--
+ALTER TABLE `autorzy`
+  MODIFY `id_autor` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT dla tabeli `ksiazka`
+--
+ALTER TABLE `ksiazka`
+  MODIFY `id_ksiazki` int(50) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT dla tabeli `tytuly`
+--
+ALTER TABLE `tytuly`
+  MODIFY `id_tytul` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `ksiazka`
+--
+ALTER TABLE `ksiazka`
+  ADD CONSTRAINT `ksiazka_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `autorzy` (`id_autor`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ksiazka_ibfk_2` FOREIGN KEY (`id_tytul`) REFERENCES `tytuly` (`id_tytul`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
